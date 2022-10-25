b6c77ea0dbc3d30a050fc5ddc90ff8339f17e0e9
LuisJann <giannoneluigi10@gmail.com>
2022-10-25 15:36:34 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -2,3 +2,14 @@
 Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Bonus:
 Aggiungere alla pagina un’immagine, presa anch’essa da un data. */
+
+const {createApp} = Vue;
+
+createApp({
+    data() {
+        return{
+            message : `Benvenuto stampato su h1 tramite Vue`,
+            image : `img/carattere-dei-cani.jpg.webp`
+        }
+    }
+}).mount("#app");
